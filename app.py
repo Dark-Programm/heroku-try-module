0d9ed26f4ae8c4a366b3334cd75012cc2fd97ecc
Heet Sheth <83351064+Dark-Programm@users.noreply.github.com>
2021-11-14 00:47:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-import Flask
+import flask
+import os
+from flask import send_from_directory
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def home():
-	return "Hello World"
+    return "Hello World"
 
 if __name__ == "__main__":
-	app.run()
+    app.secret_key = 'ItIsASecret'
+    app.debug = True
+    app.run()
